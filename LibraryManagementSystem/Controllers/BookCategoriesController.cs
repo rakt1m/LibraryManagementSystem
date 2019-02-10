9e69b4711d68d9c916eda58e6fbaba1167436748
shraktim <shahadathossainraktim@gmail.com>
2019-02-10 20:56:22 +0600

    bookcategory controller and view added

---
+++ LibraryManagementSystem/Controllers/BookCategoriesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.BLL.Contract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.DbContext.AppDbContext;
+using LibraryManagementSystem.Models.EntityModels;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BookCategoriesController : Controller
+    {
+        private readonly IBookCategoryManager _iBookCategoryManager;
+
+        public BookCategoriesController(IBookCategoryManager iBookCategoryManager)
+        {
+            _iBookCategoryManager = iBookCategoryManager;
+
+        }
+
+        // GET: BookCategories
+        public IActionResult Index()
+        {
+            return View(_iBookCategoryManager.GetAll());
+        }
+
+        // GET: BookCategories/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookCategory = _iBookCategoryManager.GetById(id);
+            if (bookCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookCategory);
+        }
+
+        // GET: BookCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create( BookCategory bookCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _iBookCategoryManager.Add(bookCategory);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookCategory);
+        }
+
+        // GET: BookCategories/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookCategory = _iBookCategoryManager.GetById(id);
+            if (bookCategory == null)
+            {
+                return NotFound();
+            }
+            return View(bookCategory);
+        }
+
+        // POST: BookCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, BookCategory bookCategory)
+        {
+            if (id != bookCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _iBookCategoryManager.Update(bookCategory);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookCategoryExists(bookCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookCategory);
+        }
+
+        // GET: BookCategories/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookCategory = _iBookCategoryManager.GetById(id);
+            if (bookCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookCategory);
+        }
+
+        // POST: BookCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var bookCategory = _iBookCategoryManager.GetById(id);
+            _iBookCategoryManager.Remove(bookCategory);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookCategoryExists(int id)
+        {
+            return _iBookCategoryManager.GetAll().Any(e => e.Id == id);
+        }
+    }
+}
---
+++ LibraryManagementSystem/Controllers/BooksController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.DbContext.AppDbContext;
+using LibraryManagementSystem.Models.EntityModels;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Books.Include(b => b.BookCategory);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.BookCategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            ViewData["BookCategoryId"] = new SelectList(_context.BookCategories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Publication,BookCategoryId")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookCategoryId"] = new SelectList(_context.BookCategories, "Id", "Id", book.BookCategoryId);
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["BookCategoryId"] = new SelectList(_context.BookCategories, "Id", "Id", book.BookCategoryId);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Publication,BookCategoryId")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookCategoryId"] = new SelectList(_context.BookCategories, "Id", "Id", book.BookCategoryId);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.BookCategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
