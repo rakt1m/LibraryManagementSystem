ad926b815c809b736b982ac010f9cd001f9c5625
shraktim <shahadathossainraktim@gmail.com>
2019-02-10 20:11:44 +0600

    Interface added in BLL

---
+++ LibraryManagementSystem.BLL/Contract/IBookManager.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManagementSystem.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.BLL.Contract
+{
+    public interface IBookManager:IManager<Book>
+    {
+    }
+}
