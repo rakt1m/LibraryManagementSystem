aa08dec46833ec41aab659ad6ed83c034bc15095
shraktim <shahadathossainraktim@gmail.com>
2019-02-15 11:07:39 +0600

    Interface repository added

---
+++ LibraryManagementSystem.Repositories/Contract/IReturnRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibraryManagementSystem.Models.EntityModels;
+
+namespace LibraryManagementSystem.Repositories.Contract
+{
+    public interface IReturnRepository:IRepository<Return>
+    {
+    }
+}
