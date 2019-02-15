d178e67300fa23056deccfa211775c670b9e5986
shraktim <shahadathossainraktim@gmail.com>
2019-02-15 18:00:31 +0600

    language controller and view added

---
+++ LibraryManagementSystem/Controllers/LanguagesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.BLL.Contract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.DbContext.AppDbContext;
+using LibraryManagementSystem.Models.EntityModels;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        private readonly ILanguageManager _iLanguageManager;
+
+        public LanguagesController(ILanguageManager iLanguageManager)
+        {
+            _iLanguageManager = iLanguageManager;
+        }
+
+        // GET: Languages
+        public IActionResult Index()
+        {
+            var language = _iLanguageManager.GetAll().ToList();
+            return View(language);
+        }
+
+        // GET: Languages/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = _iLanguageManager.GetById(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return View(language);
+        }
+
+        // GET: Languages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Languages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create( Language language)
+        {
+            if (ModelState.IsValid)
+            {
+               language.CreatedAt=DateTime.Now;
+                _iLanguageManager.Add(language);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = _iLanguageManager.GetById(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+            return View(language);
+        }
+
+        // POST: Languages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,  Language language)
+        {
+            if (id != language.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    language.UpdatedAt=DateTime.Now;
+                    _iLanguageManager.Update(language);
+                   
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LanguageExists(language.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = _iLanguageManager.GetById(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return View(language);
+        }
+
+        // POST: Languages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var language = _iLanguageManager.GetById(id);
+
+            _iLanguageManager.Remove(language);
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LanguageExists(int id)
+        {
+            return _iLanguageManager.GetAll().Any(e => e.Id == id);
+        }
+    }
+}
