8b1866e453c1808f859c66d67102d8b2172b43c7
shraktim <shahadathossainraktim@gmail.com>
2019-02-15 11:13:26 +0600

    Repository classes added

---
+++ LibraryManagementSystem.Repositories/Repository/AuthorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibraryManagementSystem.Models.EntityModels;
+using LibraryManagementSystem.Repositories.Contract;
+
+namespace LibraryManagementSystem.Repositories.Repository
+{
+  public  class AuthorRepository:Repository<Author>,IAuthorRepository
+    {
+    }
+}
